d2f4f4218be79fff6d65b8c91ad054b94c982228
gaox <350994687@qq.com>
2018-08-28 15:51:29 +0800

    dimens生成工具

---
+++ src/dimens/generator/DimenGenerator.java
@@ -0,0 +1,27 @@
+package dimens.generator;
+
+import dimens.constants.DimenTypes;
+import dimens.utils.MakeUtils;
+
+public class DimenGenerator {
+
+    /**
+     * 设计稿尺寸(将自己设计师的设计稿的宽度填入)
+     */
+    private static final int DESIGN_WIDTH = 750;
+
+    /**
+     * 设计稿的高度  （将自己设计师的设计稿的高度填入）
+     */
+    private static final int DESIGN_HEIGHT = 1920;
+
+    public static void main(String[] args) {
+        int smallest = DESIGN_WIDTH>DESIGN_HEIGHT? DESIGN_HEIGHT:DESIGN_WIDTH;  //     求得最小宽度
+        DimenTypes[] values = DimenTypes.values();
+        for (DimenTypes value : values) {
+            MakeUtils.makeAll(smallest, value, "/Users/gaox/Desktop/smallestwidth/dimens_sw");
+        }
+
+    }
+
+}
